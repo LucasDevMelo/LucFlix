32de3ea8b5a4b49cba4b936548ce028016895b49
LucasDevMelo <lucaslopesdemelosilva@gmail.com>
2023-05-19 00:59:47 -0300

    Atualização 19/05 Conectando à API

--- app/src/main/java/com/example/lucflix/MainActivity.kt
+++ app/src/main/java/com/example/lucflix/MainActivity.kt
@@ -5,18 +5,24 @@ package com.example.lucflix
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lucflix.model.Category
 import com.example.lucflix.model.Movie
+import com.example.lucflix.util.CategoryTask
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), CategoryTask.Callback {
     // m-v-c (model - [view/controller] activity)
+    private lateinit var progress: ProgressBar
+    private lateinit var adapter: CategoryAdapter
+    private val categories = mutableListOf<Category>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("Teste", "onCreate")
+        progress = findViewById(R.id.progress_main)
 
         // list vertical
         // cat 1
@@ -24,25 +30,35 @@ class MainActivity : AppCompatActivity() {
         // cat 2
         // listas h-> - f1 - f2 -f3 -f4 ...
 
-        val categories = mutableListOf<Category>()
-        for (j in 0 until 5) {
-            val movies = mutableListOf<Movie>()
-            for (i in 0 until 15) {
-                val movie = Movie(R.drawable.movie)
-                movies.add(movie)
-            }
-            val category = Category("cat $j", movies)
-            categories.add(category)
-        }
-
         // na vertical a lista (CategoryAdapter) de categorias
         // e dentro de cada item [TextView+RecyclerView horizontal]
         // (cada categoria) teremos
         // uma lista (MovieAdapter) de filmes (ImageView)
-        val adapter = CategoryAdapter(categories)
+        adapter = CategoryAdapter(categories)
         val rv: RecyclerView = findViewById(R.id.rv_main)
         rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
+
+        CategoryTask(this).execute("https://api.tiagoaguiar.co/netflixapp/home?apiKey=4407a4eb-ed0c-4700-8a41-f381ddf90e4d")
+    }
+
+    override fun onPreExecute() {
+        progress.visibility = View.VISIBLE
+    }
+
+    override fun onResult(categories: List<Category>) {
+        // aqui será quando o CategoryTask chamará de volta
+        // (callback) - listener
+        this.categories.clear()
+        this.categories.addAll(categories)
+        adapter.notifyDataSetChanged() //forca o adapter a chamar denovo o onBindViewHolder
+
+        progress.visibility = View.GONE
+    }
+
+    override fun onFailure(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        progress.visibility = View.GONE
     }
 
 }
--- app/src/main/java/com/example/lucflix/MovieActivity.kt
+++ app/src/main/java/com/example/lucflix/MovieActivity.kt
@@ -27,10 +27,6 @@ class MovieActivity : AppCompatActivity() {
         txtcast.text = getString(R.string.cast, "Ator A, ator B, ator C")
 
         val movies = mutableListOf<Movie>()
-        for (i in 0 until 15) {
-            val movie = Movie(R.drawable.movie)
-            movies.add(movie)
-        }
 
         rv.layoutManager = GridLayoutManager(this, 3)
         rv.adapter = MovieAdapter(movies, R.layout.movie_item_similar)
